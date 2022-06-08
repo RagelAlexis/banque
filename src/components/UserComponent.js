abf22613757c07dc1a274ee39bd73f0939821f5e
RagelAlexis <alexis.rageldu76@hotmail.fr>
2022-06-08 12:19:17 +0200

    revenue dépense et solde fait

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -1,9 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import React, {useState} from 'react'
 import Data from '../datas/data.json'
 import { Picker } from '@react-native-picker/picker'
+import { Avatar, Button, Card, Title, Paragraph, List } from 'react-native-paper';
 
-const selectUser = (id) => {
+
+const userId = (id) => {
     return Data.find(item => item._id === id)
 }
 
@@ -24,33 +26,65 @@ const UserComponent = () => {
     const total = (parseFloat(totalIncome) - parseFloat(totalExpenses)).toFixed(2)
 
     return (
-      <View style={styles.container}>
-       
-            <Picker
-                selectedValue={id}
-                style={{ height: 50, width: 200 }}
-                onValueChange={(item) => {
-                    setId(item)
-                    setUser(selectUser(item).user)
-                    setIncomes(selectUser(item).incomes)
-                    setExpenses(selectUser(item).expenses)
-                    setDate(selectUser(item).date)
-                    setAmount(selectUser(item).amount)
-                    setCategory(selectUser(item).category)
-                    setComments(selectUser(item).comments)
-                    set_id_income(selectUser(item)._id_income)
-                }}>
-                {Data.map(item => <Picker.Item label={item.user} value={item._id} key={item._id} />)}
-            </Picker>
-            <View style={styles.container}>
-                <Text style={styles.text}>Nom : {user}</Text>
-                <Text style={styles.text}>Revenu : {totalIncome}€</Text>
-                <Text style={styles.text}>Dépense : {totalExpenses}€</Text>
-                <Text style={styles.text}>Solde du compte : {total}€</Text>
-                <Text style={styles.text}>Détails Revenu : {incomes.map(item => item.amount.substring(1).replace(',', ''))}</Text>
-                <Text style={styles.text}>Détails Dépense : {expenses.map(item => item.amount.substring(1).replace(',', ''))}</Text>
-            </View>
+    <ScrollView style={styles.container}>
+            <Card style={styles.card}>
+                <Picker style={styles.title}
+                    selectedValue={id}
+                    onValueChange={(item) => {
+                        setId(item)
+                        setUser(userId(item).user)
+                        setIncomes(userId(item).incomes)
+                        setExpenses(userId(item).expenses)
+                        setDate(userId(item).date)
+                        setAmount(userId(item).amount)
+                        setCategory(userId(item).category)
+                        setComments(userId(item).comments)
+                        set_id_income(userId(item)._id_income)
+                    }}>
+                    {Data.map(item => <Picker.Item label={item.user} value={item._id} key={item._id} />)}
+                </Picker>
+            </Card>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{flex: 1}}>              
+            <Card style={styles.card}>
+                <Card.Title title="Revenus" />
+                <List.Section>
+                    {incomes.map((item, index) => {
+                        return (
+                            <List.Item
+                                key={index}
+                                title={item.category}
+                                description={`${item.amount.substring(1).replace(',', '')} €              Date : ${item.date.substring(0, 10)}`}          
+                            />
+                        )
+                    }
+                    )}
+                </List.Section>
+            </Card>
+        </View>  
+        <View style={{flex: 1}}>
+            <Card style={styles.card}>
+                <Card.Title title="Dépense" />
+                <List.Section>
+                    {expenses.map((item, index) => {
+                        return (
+                            <List.Item
+                                key={index}
+                                title={item.category}
+                                description={`${item.amount.substring(1).replace(',', '')} €              Date : ${item.date.substring(0, 10)}`}              
+                            />
+                        )
+                    }
+                    )}
+                </List.Section>
+            </Card>
         </View>
+        </View>
+        <Card style={styles.card}> 
+            <Card.Title title="Solde du compte" style={styles.solde}/>
+            <List.Item title={`${total} €`}/>
+        </Card>
+    </ScrollView>
     )
 }
 
@@ -66,6 +100,27 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
+        padding: 20,
+        backgroundColor: 'beige',
+      },
+      paragraph: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 20
+      },
+    title: {
+        justifyContent: 'center',
         alignItems: 'center',
     },
+    solde: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    card : {
+        borderWidth:  1,  
+        borderStyle:  'dashed',
+        marginBottom: 20,
+    }
 })
